5c004c5ce85a794f8eee19590f416829433d1ae7
Dor Galam <dorg@wix.com>
2018-03-10 16:20:25 +0200

    Runs automatically and adds cards to pile

--- part_one/scripts/index.js
+++ part_one/scripts/index.js
@@ -1,7 +1,7 @@
-var deck = [];
 
-var mainDeck = new Deck('deck', false);
-var played = new Deck('played', true);
+
+var mainDeck = new Deck('deck', false, true);
+var pile = new Deck('pile', true);
 var player = new Deck('player-one', true);
 var bot = new Deck('bot', false);
 
@@ -10,20 +10,25 @@ window.onload = function () {
   distributeCards();
   bot.render();
   player.render();
+  mainDeck.render();  
+  setInterval(() => {
+    addCardToPile(mainDeck.popCard());
+    mainDeck.render();
+  }, 500);
 }
 
 function distributeCards() {
   for (var i = 0; i < 8; ++i) {
-    player.addCard(deck.popRandomIndex());
-    bot.addCard(deck.popRandomIndex());
+    player.addCard(mainDeck.popCard());
+    bot.addCard(mainDeck.popCard());
   }
 }
 
-function Deck(elementId, isFacedUp, cardClasses = '') {
+function Deck(elementId, isFacedUp, isStack = false) {
   this.deck = [];
   this.elementId = elementId;
   this.isFacedUp = isFacedUp;
-  this.cardClasses = cardClasses;
+  this.isStack = isStack;
 }
 
 
@@ -38,11 +43,16 @@ function isPlayAble(card) {
 
 Deck.prototype.render = function () {
   var isFacedUp = this.isFacedUp;
-  var classes = this.cardClasses;
+  var isStack = this.isStack;
   document.getElementById(this.elementId).innerHTML = `
     ${
-      this.deck.map(function(card) {
-        return `<div class="card ${classes} ${isFacedUp ? `playable card_${card}` : 'card_back'}"></div>`;
+      this.deck.map(function(card, index) {
+        return `
+          <div 
+            ${isStack ? `style="margin: ${index / 3}px ${index / 3}px"` : ''} 
+            class="card ${isFacedUp ? `playable card_${card}` : 'card_back'}">
+          </div>
+        `;
       }).join('\n')
     }
   `;
@@ -52,13 +62,26 @@ Deck.prototype.addCard = function (card) {
   this.deck.push(card);
 }
 
+Deck.prototype.popCard = function() {
+  return this.deck.popRandomIndex();
+}
+
+function addCardToPile(card) {
+  pile.addCard(card);
+  document.getElementById(pile.elementId).innerHTML = `
+   ${document.getElementById(pile.elementId).innerHTML}
+    <div class="card card_${card}" 
+      style="transform: rotate(${-60 + Math.floor(Math.random() * 120)}deg); 
+      margin: ${Math.floor(Math.random() * 40)}px ${Math.floor(Math.random() * 40)}px ${Math.floor(Math.random() * 40)}px ${Math.floor(Math.random() * 40)}px "">
+    </div>
+  `;
+}
 
 function resetDeck() {
   deck = createCardsArray();
   deck.forEach(function (card) {
     mainDeck.addCard(card);
   });
-  mainDeck.render();
 }
 
 function createCardsArray() {
@@ -108,7 +131,7 @@ Array.prototype.popRandomIndex = function () {
 Array.prototype.map = function (callback) {
   var arr = [];
   for (var i = 0; i < this.length; i++) {
-    arr.push(callback(this[i]));
+    arr.push(callback(this[i], i));
   }
   return arr;
 }
